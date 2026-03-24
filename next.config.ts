01e31b9b7a8b9d1184d367c16eb286d797e527ce
Aiden Elliott <aiden@aidenish.com>
2026-03-23 23:00:36 -0700

    next hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["bigmac.local", "friday13.local"],
+};
 
 export default nextConfig;
